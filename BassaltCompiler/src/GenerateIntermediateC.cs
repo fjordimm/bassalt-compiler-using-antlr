fbf9b97d1a9d9d70fb7e0bcdc3bbb36d3b5e3f32
fjordimm <fjordimm@gmail.com>
2024-05-01 15:22:35 -0700

    got it to read input file, parse, and write to output file

---
+++ BassaltCompiler/src/GenerateIntermediateC.cs
@@ -0,0 +1,33 @@
+
+using System.IO;
+using Antlr4.Runtime.Misc;
+
+namespace BassaltCompiler
+{
+	class GenerateIntermediateC : BassaltBaseVisitor<object>
+	{
+		private readonly TextWriter outFile;
+
+		public GenerateIntermediateC(TextWriter outFile)
+		{
+			this.outFile = outFile;
+		}
+
+		public override object VisitProgram([NotNull] BassaltParser.ProgramContext context)
+		{
+			outFile.WriteLine("int main(void) {");
+			base.VisitProgram(context);
+			outFile.WriteLine("}");
+
+			return null;
+		}
+
+		public override object VisitUnit([NotNull] BassaltParser.UnitContext context)
+		{
+			outFile.WriteLine("  unito");
+			base.VisitUnit(context);
+
+			return null;
+		}
+	}
+}
--- BassaltCompiler/src/Program.cs
+++ BassaltCompiler/src/Program.cs
@@ -1,17 +1,30 @@
 ﻿
 using System;
 using System.IO;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
 
 namespace BassaltCompiler
 {
 	class Program
 	{
 		const string inFilePath = @"..\TestIO\in\in.bslt";
+		const string outFilePath = @"..\TestIO\out\out.c";
 		
 		static void Main(string[] args)
 		{
 			using TextReader inFile = File.OpenText(inFilePath);
-			Console.WriteLine(inFile.ReadToEnd());
+			using TextWriter outFile = new StreamWriter(outFilePath);
+
+			AntlrInputStream input = new AntlrInputStream(inFile);
+			BassaltLexer lexer = new BassaltLexer(input);
+			CommonTokenStream tokens = new CommonTokenStream(lexer);
+			BassaltParser parser = new BassaltParser(tokens);
+			
+			IParseTree tree = parser.program();
+
+			GenerateIntermediateC generator = new GenerateIntermediateC(outFile);
+			generator.Visit(tree);
 		}
 	}
 }
