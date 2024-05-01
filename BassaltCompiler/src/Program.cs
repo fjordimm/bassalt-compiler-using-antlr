e308529570c252b60788b46e54815de35cf6d79d
fjordimm <fjordimm@gmail.com>
2024-05-01 14:51:59 -0700

    added sln to dotnet project; changed location of Program.cs

---
+++ BassaltCompiler/src/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.IO;
+
+namespace BassaltCompiler
+{
+	class Program
+	{
+		const string inFilePath = @"..\TestIO\in\in.bslt";
+		
+		static void Main(string[] args)
+		{
+			using TextReader inFile = File.OpenText(inFilePath);
+			Console.WriteLine(inFile.ReadToEnd());
+		}
+	}
+}
